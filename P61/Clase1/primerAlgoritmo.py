40c1a036539a8eb5e514f2cb40664882b05d0655
Luis Miguel Escobar <luismiguelescobar@MacBook-Air-de-Luis.local>
2021-04-29 14:14:18 -0500

    Documentos generados en la clase 1

---
+++ P61/Clase1/primerAlgoritmo.py
@@ -0,0 +1,29 @@
+#Guardar valores
+variable_1 = int()
+variable_2 = int()
+variable_1 = 10
+variable_2 = 20
+#Realizar el cálculo y almacenarlo
+resultado = (variable_1 + variable_2)*2
+#Mostrar resultado
+print(resultado)
+#Mostrar comportamiento de tipado dinámico
+variable_1 = "hola"
+print(variable_1)
+
+a = 12
+b = 12.80
+c = 'palabra'
+d = True
+
+print( type(a)  )
+print( type(b)  )
+print( type(c)  )
+print( type(d)  )
+
+
+
+# int #Números sin parte decimal 1 3 555
+# float #Números con parte decimal 5.666
+# str #Caracteres o palabras "hola"
+# bool #Valores de verdad True False
