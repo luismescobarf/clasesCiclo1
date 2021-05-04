7656362b33431479482b4beed62424f2758fbf60
Luis Miguel Escobar <luismiguelescobar@MacBook-Air-de-Luis.local>
2021-05-03 21:00:45 -0500

    Ejercicios propuestos y punto 5 desarrollado en clase

---
+++ P45/Clase1/acumuladorDiferencias.py
@@ -0,0 +1,15 @@
+n1 = 34
+n2 = 4444
+n3 = 333
+n4 = 1000
+acumulador = 0
+d12 = n1 - n2
+acumulador = acumulador + d12
+d23 = n2 - n3
+acumulador = acumulador + d23
+d34 = n3 - n4
+acumulador = acumulador + d34
+d14 = n1 - n4
+acumulador = acumulador + d14
+#acumulador = d12 + d23 + d34 + d14
+print(acumulador)
---
+++ P45/Clase1/primerAlgoritmo.py
@@ -0,0 +1,20 @@
+variable_1 = 15
+print(type(variable_1))
+print("El primer conteido es:",variable_1)
+variable_2 = 50
+resultado = variable_1 + variable_2
+print("El resultado de la suma es: ",resultado)
+variable_1 = 14.67
+print(type(variable_1))
+print("El nuevo contenido es:",variable_1)
+variable_1 = "hola"
+print(type(variable_1))
+print("El tercer contenido es:",variable_1)
+
+
+
+
+
+
+
+
