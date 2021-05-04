1b3ea59ae185ceee22e763a661d28e68b1c08723
Luis Miguel Escobar <luismiguelescobar@MacBook-Air-de-Luis.local>
2021-05-04 18:36:56 -0500

    Desarrollo parcial Clase 2 P45

---
+++ P45/Clase2/areaCirculo.py
@@ -0,0 +1,11 @@
+#& hallar el area de un circulo
+radio = 34
+pi = 3.1416
+radioCuadrado  = radio**2
+area = pi * radioCuadrado
+print('el area del circulo es:', area)
+
+#Algoritmo
+#Entradas: radio
+#Proceso: cálculo pi*r*r
+#Reportar la salida
---
+++ P45/Clase2/distanciaPuntos2D.py
@@ -0,0 +1,26 @@
+#Programa que calcula la distancia de dos coordenadas en el plano cartesiano
+
+#Objetivo => Usar la formula euclidiana para hallar la distancia entre dos coordenadas en el plano cartesiano
+
+""" Pasos para solucionarlo
+
+1.Definir y almacenar el valor de X1
+2.Definir y almacenar el valor de Y1
+3.Definir y almacenar el valor de X2
+4.Definir y almacenar el valor de Y2
+5.Aplicar y almacenar el resultado de la formula euclidiana
+5.Formula euclidiana (((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** (1/2)
+6.Mostrar el resultado
+"""
+"""
+0) Los paréntesis -> Cambiar el orden!!!
+1) Potencias (Primero de derecha a izquierda)
+2) Multiplicaciones y las divisiones (Primero de izquierda a derecha)
+3) Sumas y restas (Primero de izquierda a derecha)
+"""
+X1 = 4
+Y1 = 5
+X2 = 6
+Y2 = 5
+formula = (((X1 - X2) ** 2) + ((Y1 - Y2) ** 2)) ** (1/2)
+print("La distacia es: ", formula)
