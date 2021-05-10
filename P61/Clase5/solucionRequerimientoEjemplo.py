829990ed977bba7a2533b4c0ae29929c3df0daaf
luismescobarf <luismescobarf@gmail.com>
2021-05-10 11:31:41 -0500

    Solucion al requerimiento: DF

---
+++ P61/Clase5/solucionRequerimientoEjemplo.py
@@ -0,0 +1,14 @@
+#Objetivo: detectar mayores de edad
+#para realizar el informe correspondiente
+#al requerimiento.
+
+#Algoritmo pseudocódigo general
+1) Recoger 4 edades y almacenarlas.
+2) Recoger la edad vigente como mayoría
+de edad.
+3) Verificar quiénes son mayores de edad 
+y contarlos (acumulador).
+4) Si hay por lo menos dos mayores de edad,
+calcular el promedio de edad de estos.
+5) Dependiendo del número de mayores de edad
+realizar reporte acumulado de mayores y promedio.
