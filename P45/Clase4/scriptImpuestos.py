2720490557fd2bc03c79c57c17319e71b939875c
luismescobarf <luismescobarf@gmail.com>
2021-05-10 18:26:34 -0500

    Inicio clase 4 P45 definicion de requerimiento

---
+++ P45/Clase4/scriptImpuestos.py
@@ -0,0 +1,19 @@
+#Objetivo -> Precio final de venta de productos cargados
+#sin IVA
+
+#Algoritmo:
+# 1) Cargar el precio sin IVA de cada producto.
+# 2) Cargar el valor del IVA (porcentaje, float)
+# 3) Inicializamos un acumulador para el impuesto de cada 
+# producto
+# 4) Calcular el IVA del primer producto y sumarlo al 
+# acumulador
+# 5) Calcular el IVA del segundo producto y sumarlo al 
+# acumulador
+# 6) Calcular el IVA del tercer producto y sumarlo al 
+# acumulador
+# 7) Calcular el IVA del cuarto producto y sumarlo al 
+# acumulador
+# 8) Calculamos el valor total de los productos
+# 9) Sumamos al valor total de los productos el acumulado
+# de impuestos.
