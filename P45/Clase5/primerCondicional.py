149f63a97f91263cf9635c6e8fb64dcb5cadd293
luismescobarf <luismescobarf@gmail.com>
2021-05-11 18:31:13 -0500

    Primer script con condicionales

---
+++ P45/Clase5/primerCondicional.py
@@ -0,0 +1,22 @@
+#Requerimiento: Programa que recibe dos números enteros y reporta
+#si la suma de ellos es positiva.
+
+#Objetivo -> Reportar solamente si es positiva la suma
+#de los números ingresados
+
+#Agloritmo (pseudocódigo)
+# 1) Cargar desde el teclado los valores de los números
+# 2) Sumamos y guardamos el resultado
+# 3) Si el resultado de la suma es positivo
+#     3a) Reportamos
+
+#Traducción
+a = int(input('Ingrese el primer valor: '))
+b = int(input('Ingrese el segundo valor: '))
+c = a + b
+if c > 0:
+    print("La sumatoria es positiva")
+
+
+
+
