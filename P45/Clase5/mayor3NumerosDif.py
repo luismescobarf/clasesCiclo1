547f0f840f31e04bfb13f0f04b67804fc1d20349
luismescobarf <luismescobarf@gmail.com>
2021-05-12 08:31:01 -0500

    Primer ejemplo condicionales clase

--- P45/Clase5/mayor3NumerosDif.py
+++ P45/Clase5/mayor3NumerosDif.py
@@ -7,4 +7,19 @@
 #Representación -> Diagrama de flujo
 
 #Traducción -> Python
+print("Ingrese 3 números diferentes")
+a = int(input())
+b = int(input())
+c = int(input())
+if a>b:
+    if a>c:
+        print(a,"es el mayor número")
+    else:
+        print(c,"es el mayor número")
+else:
+    if b>c:
+        print(b,"es el mayor número")
+    else:
+        print(c,"es el mayor número")
+
 
