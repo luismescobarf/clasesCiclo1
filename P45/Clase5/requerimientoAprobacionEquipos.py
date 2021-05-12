e097b0ee751b8819fb66ed92089ac8abd77705b1
luismescobarf <luismescobarf@gmail.com>
2021-05-11 19:22:47 -0500

    Diagrama de Flujo solucion compartida por Daniel

--- P45/Clase5/requerimientoAprobacionEquipos.py
+++ P45/Clase5/requerimientoAprobacionEquipos.py
@@ -20,18 +20,25 @@ def PedirDatos():
 def PersonasMayores(Edad1, Edad2, Edad3, Edad4):
     PersonasMayoresDeEdad = 0
     PromedioEdad = 0
+
     if Edad1 >= 18:
-        PersonasMayoresDeEdad += 1
-        PromedioEdad += Edad1
+        # PersonasMayoresDeEdad += 1
+        # PromedioEdad += Edad1
+        PersonasMayoresDeEdad = PersonasMayoresDeEdad + 1
+        PromedioEdad = PromedioEdad + Edad1
+
     if Edad2 >= 18:
         PersonasMayoresDeEdad += 1
         PromedioEdad += Edad2
+
     if Edad3 >= 18:
         PersonasMayoresDeEdad += 1
         PromedioEdad += Edad3
+
     if Edad4 >= 18:
         PersonasMayoresDeEdad += 1
         PromedioEdad += Edad4
+
     return PersonasMayoresDeEdad, PromedioEdad
 
 def Resultado(PersonasMayoresDeEdad,Nombre1,Edad1,Nombre2,Edad2, Nombre3, Edad3, Nombre4,Edad4, PromedioEdad, Equipo):
