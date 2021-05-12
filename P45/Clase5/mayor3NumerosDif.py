ff7c2df57acdb4c6290982197f6a985fa2cf9bf4
luismescobarf <luismescobarf@gmail.com>
2021-05-11 20:22:01 -0500

    Diagrama de flujo mayor de 3 numeros diferentes

---
+++ P45/Clase5/mayor3NumerosDif.py
@@ -0,0 +1,10 @@
+#Se requiere una función que reciba 3 números
+#que son diferentes, y determine cuál es el mayor de ellos.
+#Observación -> Los números no están ordenados
+
+#Objetivo -> Reportar el mayor número
+
+#Representación -> Diagrama de flujo
+
+#Traducción -> Python
+
