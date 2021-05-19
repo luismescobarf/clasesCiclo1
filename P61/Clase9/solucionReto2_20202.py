d86a8a69faa994ae677840ad9316ecc737c61467
luismescobarf <luismescobarf@gmail.com>
2021-05-19 08:03:19 -0500

    Inicio Clase 9 P61

---
+++ P61/Clase9/solucionReto2_20202.py
@@ -0,0 +1,21 @@
+#Objetivo -> Retornar diccionario con el id del préstamo y el campo booleano
+#'aprobado' con el valor de verdad correspondiente.
+
+#Algoritmo
+#1) Procesar entradas (estandarización) para comparaciones posteriores.
+#2) Implementar árbol de decisiones para aprobar o no el préstamo
+#3) Retornar el diccionario solicitado.
+
+#Copias (conservar la data original)
+id_prestamo =  informacion['id_prestamo']
+# casado = False
+# if informacion['casado'] == 'Si':
+#     casado = True
+casado = informacion['casado'] == 'Si'
+
+
+# variable = cp < 180
+# return {'id_prestamo':informacion['id_prestamo'], 'aprobado':variable}
+
+
+
