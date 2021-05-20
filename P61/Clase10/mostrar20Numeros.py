1dff19a96d56b651dc049e519e1c4e5f44103930
luismescobarf <luismescobarf@gmail.com>
2021-05-20 10:21:24 -0500

    Primer while

---
+++ P61/Clase10/mostrar20Numeros.py
@@ -0,0 +1,24 @@
+#Requerimiento: mostrar en pantalla los números
+#del 1 al 20.
+
+#Objetivo: El mismo requerimiento
+
+# #Traducción Python:
+# print(1)
+# print(2)
+# print(3)
+# .
+# .
+# .
+
+#Aprovechamos la flexibilidad del while
+# x = 1
+# while(x<=20):
+#     print(x,end=" ")
+#     x += 1
+
+#Acercar la notación del while al for
+x = 1
+while(x<=20):
+    print(x,end=" ")
+    x += 1
