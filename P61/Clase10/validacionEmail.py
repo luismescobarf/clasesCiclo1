3a1b2cef3bc34b4d9be2a0bfe97e83050cb57371
luismescobarf <luismescobarf@gmail.com>
2021-05-20 11:42:21 -0500

    Requerimiento inicial email

---
+++ P61/Clase10/validacionEmail.py
@@ -0,0 +1,15 @@
+#Requerimiento: El usuario ingresa un correo electrónico
+#se necesita una primera función (esto es solamente una
+#etapa de la validación) que revise que el número de arrobas
+#sea válildo (estrictamente igual a 1)
+
+#Objetivo -> reportar si es válido o no el número de arrobas
+#del correo
+
+#Algoritmo
+#1) Capturar el correo electrónico que solicita el usuario
+#2) Recorrer los caracteres del correo capturado
+#2a) Si el caracter es igual a arroba, acumular en el contador
+#de arrobas
+#3) Revisar el contador de arrobas, si es igual a 1 reportar validez
+#3a) Si es diferente de uno reportar invalidez del correo
