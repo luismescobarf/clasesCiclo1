7cb2985b357c6277f00d9df1dd682e63d858b0e4
luismescobarf <luismescobarf@gmail.com>
2021-05-19 20:55:08 -0500

    Intro a listas, modificación y anidación inicial

---
+++ P45/Clase9/introduccionListas.py
@@ -0,0 +1,57 @@
+#Listas -> Primer contenedor general
+#Mutable -> cada casilla la podemos modificar
+#CRUD -> Create, Read, Update, Delete
+#En cada casilla podemos guardar información de cualquier tipo
+#Cada posición tiene un índice (autonumérico)
+
+#Ejemplo lista
+lista = [12,56,7.9,'Hola Mundo']
+
+#Requerimiento -> El usuario va a especificar el número
+#de ingresos que va a realizar. Se va a identificar de
+#esas entradas cuántos y cuáles de ellos son números pares
+#y cuáles son impares. Además necesitamos saber a cuál de las
+#entradas corresponde
+
+#Objetivo -> Informar cuántos y cuales son pares e impares
+#de las entradas recibidas, y en qué momento de la secuencia
+#fueron ingresados
+
+#Algoritmo
+#1) Solicitar al usuario el número de entradas que va a realizar
+#2) Por cada una de las entradas revisar si es número par
+#3) Si es primo coleccionarlo
+#3b) De lo contrario continuar solicitando las entradas  
+
+#Trducción
+n = int(input('Cuántos números va a ingresar?'))
+coleccionNumerosPares = []
+coleccionNumerosImpares = []
+for i in range(n):
+    num = int(input('Ingrese Número: ')) 
+    if num % 2 == 0:
+        #coleccionNumerosPares.append(num)
+        coleccionNumerosPares.append([num,i])        
+    else:
+        #coleccionNumerosImpares.append(num)
+        coleccionNumerosImpares.append([num,i])        
+
+soloNumerosPares = []
+for numero in coleccionNumerosPares:
+    soloNumerosPares.append(numero[0])
+
+soloNumerosImpares = []
+for numero in coleccionNumerosImpares:
+    soloNumerosImpares.append(numero[0])
+
+print(f"Número Pares Ingresados : {len(coleccionNumerosPares)} Colección: {soloNumerosPares}")
+print(f"Número Impares Ingresados : {len(coleccionNumerosImpares)} Colección: {soloNumerosImpares}")
+
+
+
+
+
+
+
+
+
