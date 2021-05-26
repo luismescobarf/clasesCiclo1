1a73dbc88bde3dfc9242a6652282d4656ec88613
luismescobarf <luismescobarf@gmail.com>
2021-05-26 10:01:38 -0500

    Cierre de la sesión 12 P61

--- P61/Clase12/ejemplosListasCompuestas.py
+++ P61/Clase12/ejemplosListasCompuestas.py
@@ -3,12 +3,13 @@ bdFrutas = [
     ['Pera',1200,'Importada',56],
     ['Manzana',780,57],
     ['Borojó',800,57], 
-    ['Uva',4000,'Importada',51],       
-    ['Mango',1100,57],
+    ['Uva',4000,'Importada',51, ['Olímpica','Abastos'] ],       
+    ['Mango',1100,57,'Nacional'],
     ['Importado','Banano',2000,59],
-    [2000,'Feijoa',57],
-    [57,'Mandarina',600],
-    ['Kiwi']     
+    [2000,'Feijoa',57, ['Exito'] ],
+    [57,'Mandarina',600,['Mercasa','Inter','Carulla']],
+    ['Kiwi','No tengo valores'],
+    [1800]
 ]
 
 #Recorrer la base de datos con subíndices
@@ -43,20 +44,57 @@ for i,fruta in enumerate(bdFrutas):
 # atributo tipo string.
 
 #Algoritmo:
-# 1) Para cada fruta de la base de datos
-# 2)  Para cada atributo de la fruta
+# 1) Para cada fruta de la base de datos:
+# 2)  Para cada atributo de la fruta:
 # 3)      Si el atributo es tipo string:
 # 4)           Si no se han incluído aún atributos tipo string:
 # 5)              Incorporar el atributo
 # 5)      De lo contrario:
 # 6)          Incorporar el atributo
 
+#Traducción a Python
+
+# #Modificación de la base de datos
+# for i,fruta in enumerate(bdFrutas):
+#     contadorCadenas = 0
+#     nuevaVersionFruta = list()
+#     for atributo in fruta:
+#         if isinstance(atributo,str):
+#             if contadorCadenas == 0:
+#                 nuevaVersionFruta.append(atributo)
+#                 contadorCadenas += 1
+#         else:
+#             nuevaVersionFruta.append(atributo)
+#     bdFrutas[i] = nuevaVersionFruta
+
+def atributoStringUnico(bd):
+    for i,fruta in enumerate(bd):
+        contadorCadenas = 0
+        nuevaVersionFruta = list()
+        for atributo in fruta:
+            if isinstance(atributo,str):
+                if contadorCadenas == 0:
+                    nuevaVersionFruta.append(atributo)
+                    contadorCadenas += 1
+            else:
+                nuevaVersionFruta.append(atributo)
+        bd[i] = nuevaVersionFruta
+        return bd
 
 
+#Sección principal
+atributoStringUnico(list(bdFrutas))
+#atributoStringUnico(bdFrutas.copy())
 
-    
-        
+#Recorrer la base de datos (minimizando subíndices)
+print('-----------------------------')
+for i,fruta in enumerate(bdFrutas):
+    cadenaSalidaFruta = str()
+    cadenaSalidaFruta += f"Fruta {i+1}) "
+    for j,atributo in enumerate(fruta):
+        cadenaSalidaFruta += f"Atributo{j+1}: {atributo}, "
+    print(cadenaSalidaFruta)
 
-
+#Propuesto -> Metodologías propuestas en clase para el requerimiento (eliminación)
 
 
