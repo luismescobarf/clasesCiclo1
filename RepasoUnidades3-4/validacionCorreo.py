23cf1578d9b27641882190aed67204500163a4c4
luismescobarf <luismescobarf@gmail.com>
2021-06-05 09:38:23 -0500

    Actualizacion

--- RepasoUnidades3-4/validacionCorreo.py
+++ RepasoUnidades3-4/validacionCorreo.py
@@ -1,29 +1,49 @@
 #Requerimiento -> Determinar si un correo tiene le número de arrobas adecuado (1)
-#Obtener el usuario y el dominio si es válido.
-#Solicitar varios correos y almacenar en una lista de diccionarios aquellos que son válidos
+#Obtener el usuario y el dominio si es válido (2)
+#(3) Solicitar varios correos y almacenar en una lista de diccionarios aquellos que son válidos
 
-#Solicitar correo al usuario
-correo = input('Ingresar correo: ')
-numArrobas = 0
-# for caracter in correo:
-#     if caracter == '@':
-#         numArrobas += 1    
-for i in range(len(correo)):
-    if correo[i] == '@':
-        numArrobas += 1
-elementosCorreo = list()
-diccionarioCorreo = dict()
-if numArrobas == 1:
-    print('Correo válido')
-    elementosCorreo = correo.split('@')
-    #Alternativa de actualización de diccionario
-    # diccionarioCorreo['Nombre Usuario'] = elementosCorreo[0]
-    # diccionarioCorreo['Dominio'] = elementosCorreo[1]       
-    diccionarioCorreo =  {'Nombre Usuario':elementosCorreo[0],'Dominio':elementosCorreo[1]}
-else:
-    print('Correo inválido')
+def correoValido(correo):
+    numArrobas = 0
+    # for caracter in correo:
+    #     if caracter == '@':
+    #         numArrobas += 1    
+    for i in range(len(correo)):
+        if correo[i] == '@':
+            numArrobas += 1
+    if numArrobas == 1:
+        return True
+    else:
+        return False
 
-#Guardad la información en un diccionario mostrarla en pantalla
-print('Accediendo a cada posición:')
-print("Nombre de usuario: ", diccionarioCorreo['Nombre Usuario'])
-print("Dominio: ",diccionarioCorreo['Dominio'])
+#Solicitar 3 correos y clasificarlos -> guardarlos en una lista de diccionarios
+listaCorreosValidos = list()
+for _ in range(3):
+    #Solicitar correo al usuario
+    correo = input('Ingresar correo: ')
+    elementosCorreo = list()
+    diccionarioCorreo = dict()
+    if correoValido(correo):
+        #print('Correo válido')
+        elementosCorreo = correo.split('@')
+        #Alternativa de actualización de diccionario
+        # diccionarioCorreo['Nombre Usuario'] = elementosCorreo[0]
+        # diccionarioCorreo['Dominio'] = elementosCorreo[1]       
+        diccionarioCorreo =  {'Nombre Usuario':elementosCorreo[0],'Dominio':elementosCorreo[1]}
+        listaCorreosValidos.append(diccionarioCorreo)
+    else:
+        print('Correo inválido')
+
+#Mostrar contenedor final (colección de correos válidos)
+listaCorreosValidos = tuple(listaCorreosValidos)
+print(listaCorreosValidos)
+
+#Recorrido contenedor de contenedores
+for i,correo in enumerate(listaCorreosValidos):
+    print("---------------------------")
+    print(f"Correo Válido No. {i+1}")
+    for llave,valor in correo.items():
+        print(f"{llave}: {valor}")
+        
+
+
+
