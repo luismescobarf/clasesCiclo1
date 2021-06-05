c563ee2274cf28561e738c40891b5649ebfe61f2
luismescobarf <luismescobarf@gmail.com>
2021-06-05 08:21:28 -0500

    Actualizacion Carpeta

---
+++ RepasoUnidades3-4/ciclos.py
@@ -0,0 +1,12 @@
+#R:Imprimir los primeros 20 números
+# https://github.com/luismescobarf/clasesCiclo1/tree/master/RepasoUnidades3-4
+
+#While -> Se recomienda cuando necesitamos mayor generalidad
+x = 1
+while(x<=20):
+    print(x,end='-')
+    x += 1
+
+
+
+ 
