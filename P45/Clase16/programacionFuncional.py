32720a85589d9eb499973c77ceff491719bde94e
luismescobarf <luismescobarf@gmail.com>
2021-06-07 18:40:23 -0500

    Actualizacion

---
+++ P45/Clase16/programacionFuncional.py
@@ -0,0 +1,43 @@
+#Híbrido Programación Funcional en Python:
+#1) Minimizar las asignaciones a variables (cambios de estado de las variables)
+#2) Tratamos a las funciones como variables: - Funciones de primera clase
+#                                            - Funciones de orden superior
+#3) Minimizar el acceso a estados globales
+
+#Calculadora estilo programación funcional
+def operacion(signo):
+    if signo == '+':
+        def suma(a=0,b=0):
+            return a + b
+        return suma
+    elif signo == '-':
+        def resta(a=0,b=0):
+            return a - b
+        return resta
+    elif signo == '*':
+        def multiplicacion(a=0,b=0):
+            return a * b
+        return multiplicacion
+    elif signo == '/':
+        def division(a=0,b=1):
+            return a // b
+        return division
+    else:
+        def f(a=0,b=0):
+            return (a,b)
+        return f
+
+def calculadora(signo, a, b):
+
+    #Construir o solicitarle a otra función la construcción del proceso que debe hacer
+    funcionAplicar = operacion(signo)
+
+    #Aplicar función que cunstruída en tiempo de ejecución
+    return funcionAplicar(a,b)
+
+#Utilizar función
+print(calculadora('+',7,8))
+print(calculadora('-',-12,8))
+print(calculadora('*',1555,4))
+print(calculadora('/',88,2))
+print(calculadora('akjsdlkajsdlk',7,8))
