edc27802a57d70fe64a715c3ffbf4a7294dba422
luismescobarf <luismescobarf@gmail.com>
2021-06-08 18:12:40 -0500

    Sesion 17 P45

---
+++ P45/Clase17/ejercicioPropuesto.py
@@ -0,0 +1,11 @@
+from casosGenerados import *
+
+#1)Obtener todas las transferencias realizadas en el último trimestre en los cajeros
+#de modelo 101,2017
+#2)Obtener todas las consignaciones realizadas en el último trimestre en los cajeros
+#de modelo 101,2017 que se encuentran fuera de servicio
+#3) Todas las transacciones de los cajeros cerrados que pertenecen a la firma integrada
+# -> Convencional: Ciclando y con condicionales
+# -> Funcional: Map, Filter, Zip o Reduce
+
+print(caso1['25u8sBP']['transacciones'][0])
