9406171677ee8d8d2e545ac461f588cd5d02e1e7
luismescobarf <luismescobarf@gmail.com>
2021-06-09 20:10:05 -0500

    Coleccionando con funciones de primera clase y de orden superior

--- P45/Clase18/ejemplosParadigmaFuncional.py
+++ P45/Clase18/ejemplosParadigmaFuncional.py
@@ -20,36 +20,74 @@
 # 3) Aplicar el corrimiento a cada una de las componentes
 
 #Totalmente Procedural estructurado
+# n = 3
+# corrimientoX = 4
+# corrimientoY = 1
+# coleccionPuntos = []
+# for _ in range(n):
+#     coordenada = input('Ingrese coordenadas: ')
+#     coordenada = coordenada.split(' ')
+#     coordenada[1] = float(coordenada[1])
+#     coordenada[2] = float(coordenada[2])
+#     coleccionPuntos.append(coordenada)
+# print('Colección de puntos')
+# print(coleccionPuntos)
+
+# for i in range(len(coleccionPuntos)):
+#     coleccionPuntos[i][1] = coleccionPuntos[i][1] + corrimientoX
+# print('Colección con corrimiento en x aplicado:')
+# print(coleccionPuntos)
+
+# for i in range(len(coleccionPuntos)):
+#     coleccionPuntos[i][2] = coleccionPuntos[i][2] + corrimientoY
+# print('Colección con corrimiento en y aplicado:')
+# print(coleccionPuntos)
+
+#Funciones de primera clase y de orden superior
+
+#Primera clase
+def convertirFormatoSFF(coordenada):
+    return [coordenada[0],float(coordenada[1]),float(coordenada[2])]
+
+def convertirFormatoSIF(coordenada):
+    return [coordenada[0],int(coordenada[1]),float(coordenada[2])]
+
+def convertirFormatoSII(coordenada):
+    return [coordenada[0],int(coordenada[1]),int(coordenada[2])]
+
+#Partir la coordenada entrada como texto
+def partirPorEspacios(coordenada):
+    return coordenada.split(' ')
+
+def partirPorComas(coordenada):
+    return coordenada.split(',')
+
+#Acumular en un alista las coordenadas
+def coleccionarEnLista(lista, elemento):
+    #return lista + elemento
+    return lista.append(elemento)
+
+def coleccionarEnDiccionario(diccionario, elemento):
+    diccionario[elemento[0]+str(elemento[1])+str(elemento[2])] = elemento
+
+#Función para coleccionar las coordenadas (función de orden superior)
+def coleccionarCoordenadas(cadenaCoordenada,coleccion,funcionPartido,funcionFormato,funcionColeccion):
+    funcionColeccion(coleccion, funcionFormato(funcionPartido(cadenaCoordenada)) )    
+
 n = 3
 corrimientoX = 4
 corrimientoY = 1
-coleccionPuntos = []
+#coleccionPuntos = list()
+coleccionPuntos = dict()
 for _ in range(n):
-    coordenada = input('Ingrese coordenadas: ')
-    coordenada = coordenada.split(' ')
-    coordenada[1] = float(coordenada[1])
-    coordenada[2] = float(coordenada[2])
-    coleccionPuntos.append(coordenada)
-print('Colección de puntos')
+    #coleccionarCoordenadas(input('Coord: '),coleccionPuntos,partirPorEspacios,convertirFormatoSFF,coleccionarEnLista)
+    coleccionarCoordenadas(input('Coord: '),coleccionPuntos,partirPorComas,convertirFormatoSII,coleccionarEnDiccionario)
+print('Colección de puntos obtenida')
 print(coleccionPuntos)
-
-for i in range(len(coleccionPuntos)):
-    coleccionPuntos[i][1] = coleccionPuntos[i][1] + corrimientoX
-print('Colección con corrimiento en x aplicado:')
-print(coleccionPuntos)
-
-for i in range(len(coleccionPuntos)):
-    coleccionPuntos[i][2] = coleccionPuntos[i][2] + corrimientoY
-print('Colección con corrimiento en y aplicado:')
-print(coleccionPuntos)
+    
 
 
 
-
-
-
-
-#Funciones de primera clase y de orden superior
 
 #Aprovechando map y reduce
 
