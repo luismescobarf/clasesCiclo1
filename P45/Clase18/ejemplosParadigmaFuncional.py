7da29db74d91066738e4f76f31b6fc127faa1749
luismescobarf <luismescobarf@gmail.com>
2021-06-09 19:14:53 -0500

    Ejemplo funcional

---
+++ P45/Clase18/ejemplosParadigmaFuncional.py
@@ -0,0 +1,18 @@
+#Requerimiento: se le solicita al usuario n coordenadas
+#por teclado. Debe ingresarlas colocando primero el nombre del 
+#punto que representa la coordenada, y a continuación los valores
+#en x y y que componen la coordenada. Coleccionarlas y al final
+#aplicar un corrimiento dado a todas las coordenadas.
+
+#Ejemplo:
+# 'Bogotá' 12.5 4.8
+# 'A' 7 10
+
+# Corrimiento en X -> 1
+# Corrimiento en Y -> 2
+
+# 'Bogotá'    13.5    6.8
+# 'A'         8       12
+
+#Algoritmo
+
